ee28936016b6fd5ce8b492353afd029588992c18
OK-Ro <o.robert1994@hotmail.com>
2024-06-08 11:20:32 +0200

    contatc

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,103 +1,118 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
-const Section = styled.section`
-  background-color: #f8f9fa;
-  padding: 200px 0;
+const FooterSection = styled.footer`
+  background-color: #333;
+  color: #fff;
+  padding: 40px 0;
+`;
 
-  @media (max-width: 768px) {
-    padding: 100px 0;
+const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
   }
 `;
 
-const AboutContainer = styled.div`
+const FooterLinks = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0 0 20px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (min-width: 768px) {
+    flex-direction: row;
+    margin: 0;
+  }
+`;
+
+const FooterLink = styled.li`
+  margin: 0 15px;
+  a {
+    color: #fff;
+    text-decoration: none;
+    &:hover {
+      color: #ccc;
+    }
+  }
+`;
+
+const ContactInfo = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+  @media (min-width: 768px) {
+    text-align: left;
+    margin-bottom: 0;
+  }
+  p {
+    margin: 5px 0;
+  }
+`;
+
+const SocialLinks = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-`;
-
-const AboutContent = styled.div`
-  flex: 1;
-  max-width: 800px;
-  text-align: center;
-`;
-
-const AboutHeading = styled.h2`
-  font-size: 4rem;
-  color: #333;
-  margin-bottom: 20px;
-
-  @media (max-width: 768px) {
-    font-size: 3rem;
+  .social-icon {
+    margin: 0 10px;
+    color: #fff;
+    font-size: 24px;
+    text-decoration: none;
+    transition: color 0.3s;
+    &:hover {
+      color: #ccc;
+    }
   }
 `;
 
-const AboutDescription = styled.p`
-  font-size: 1.6rem;
-  color: #555;
-  line-height: 1.6;
-
-  @media (max-width: 768px) {
-    font-size: 1.4rem;
-  }
-`;
-
-const AboutImageLeft = styled.img`
-  width: 35%;
-  height: 40vh;
-  margin-right: 40px;
-  object-fit: cover;
-
-  @media (max-width: 768px) {
-    width: 100%;
-    margin-right: 0;
-    margin-bottom: 20px;
-    height: auto;
-  }
-`;
-
-const AboutImageRight = styled.img`
-  width: 35%;
-  height: 40vh;
-  margin-left: 40px;
-  object-fit: cover;
-
-  @media (max-width: 768px) {
-    width: 100%;
-    margin-left: 0;
-    margin-top: 20px;
-    height: auto;
-  }
-`;
-
-const AboutSection = () => {
+const Footer = () => {
   return (
-    <Section id="about">
-      <AboutContainer>
-        <AboutImageLeft
-          src="https://www.bing.com/th?id=OADD2.8864818430287_1S15A0UFLNE8VI0IJV&pid=21.2&c=16&roil=0&roit=0&roir=1&roib=1&w=472&h=247&rs=2"
-          alt="Left"
-        />
-        <AboutContent>
-          <AboutHeading>About Us</AboutHeading>
-          <AboutDescription>
-            Website Bank is your one-stop destination for high-quality website
-            templates. Whether you're a business owner looking to establish an
-            online presence, a freelancer showcasing your portfolio, or an
-            entrepreneur starting an e-commerce venture, we have the perfect
-            template for you. Our templates are professionally designed, fully
-            customizable, and optimized for performance. With Website Bank,
-            creating your dream website has never been easier.
-          </AboutDescription>
-        </AboutContent>
-        <AboutImageRight
-          src="https://th.bing.com/th/id/OIP.Vj8z4bemjSqAtnFXtjVaOwHaFX?w=268&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-          alt="Right"
-        />
-      </AboutContainer>
-    </Section>
+    <FooterSection>
+      <FooterContainer>
+        {/* Footer Links */}
+        <FooterLinks>
+          <FooterLink>
+            <a href="/#">Terms of Service</a>
+          </FooterLink>
+          <FooterLink>
+            <a href="/#">Privacy Policy</a>
+          </FooterLink>
+          <FooterLink>
+            <a href="/#">FAQs</a>
+          </FooterLink>
+        </FooterLinks>
+
+        {/* Contact Information */}
+        <ContactInfo>
+          <p>Contact Us:</p>
+          <p>Email: info@example.com</p>
+          <p>Phone: 123-456-7890</p>
+        </ContactInfo>
+
+        {/* Social Media Links */}
+        <SocialLinks>
+          <a href="/#" className="social-icon">
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+          <a href="/#" className="social-icon">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a href="/#" className="social-icon">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </SocialLinks>
+      </FooterContainer>
+    </FooterSection>
   );
 };
 
-export default AboutSection;
+export default Footer;
