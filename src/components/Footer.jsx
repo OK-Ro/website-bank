50e519724adc00cd2b84b433fbf68ef7b1cda9da
OK-Ro <o.robert1994@hotmail.com>
2024-06-08 12:34:41 +0200

    contatc

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,55 +1,242 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFacebookF,
-  faTwitter,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+import styled, { keyframes } from "styled-components";
+
+const bubbleSize = keyframes`
+  0%, 75% {
+    width: var(--size, 4rem);
+    height: var(--size, 4rem);
+  }
+  100% {
+    width: 0rem;
+    height: 0rem;
+  }
+`;
+
+const bubbleMove = keyframes`
+  0% {
+    bottom: -4rem;
+  }
+  100% {
+    bottom: var(--distance, 10rem);
+  }
+`;
+
+const FooterSection = styled.footer`
+  grid-area: footer;
+  position: relative;
+  height: 22rem;
+  background: #000;
+  z-index: 1;
+`;
+
+const Bubbles = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 1rem;
+  background: transparent;
+
+  .bubble {
+    position: absolute;
+    left: var(--position, 50%);
+    width: var(--size, 4rem);
+    height: var(--size, 4rem);
+    background: radial-gradient(
+      ellipse at center,
+      rgba(255, 87, 34, 0.8) 0%,
+      rgba(255, 87, 34, 0.6) 20%,
+      rgba(255, 87, 34, 0.4) 40%,
+      rgba(255, 87, 34, 0.2) 60%,
+      transparent 80%
+    );
+    z-index: 1;
+    border-radius: 10% 50% 50% 50% / 50% 10% 50% 50%;
+    animation: ${bubbleSize} var(--time, 4s) ease-in infinite var(--delay, 0s),
+      ${bubbleMove} var(--time, 4s) ease-in infinite var(--delay, 0s);
+    transform: translate(-50%, 100%);
+  }
+`;
+
+const Content = styled.div`
+  z-index: 2;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: 4rem;
+  padding: 2rem;
+  background: #000;
+  padding-top: 9rem;
+
+  a,
+  p {
+    color: #f5f7fa;
+    text-decoration: none;
+  }
+
+  b {
+    color: white;
+  }
+
+  p {
+    margin: 0;
+    font-size: 0.75rem;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    > div {
+      margin: 0.25rem 0;
+
+      > * {
+        margin-right: 0.5rem;
+      }
+    }
+
+    .image {
+      align-self: center;
+      width: 4rem;
+      height: 4rem;
+      margin: 0.25rem 0;
+      background-size: cover;
+      background-position: center;
+    }
+  }
+`;
 
 const Footer = () => {
+  const bubbles = Array.from({ length: 128 }, (_, i) => ({
+    size: `${2 + Math.random() * 4}rem`,
+    distance: `${6 + Math.random() * 4}rem`,
+    position: `${-5 + Math.random() * 110}%`,
+    time: `${2 + Math.random() * 2}s`,
+    delay: `${-1 * (2 + Math.random() * 2)}s`,
+  }));
+
   return (
-    <footer className="bg-gray-800 text-white py-10">
-      <div className="container mx-auto px-6 md:px-12 flex flex-col md:flex-row justify-between items-center">
-        {/* Footer Links */}
-        <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 mb-6 md:mb-0">
-          <li>
-            <a href="/#" className="hover:text-gray-400">
-              Terms of Service
+    <FooterSection>
+      <Bubbles>
+        {bubbles.map((bubble, index) => (
+          <div
+            key={index}
+            className="bubble"
+            style={{
+              "--size": bubble.size,
+              "--distance": bubble.distance,
+              "--position": bubble.position,
+              "--time": bubble.time,
+              "--delay": bubble.delay,
+            }}
+          ></div>
+        ))}
+      </Bubbles>
+      <Content>
+        <div>
+          <div>
+            <b>Eldew</b>
+            <a href="#" aria-label="Secuce">
+              Secuce
             </a>
-          </li>
-          <li>
-            <a href="/#" className="hover:text-gray-400">
-              Privacy Policy
+            <a href="#" aria-label="Drupand">
+              Drupand
             </a>
-          </li>
-          <li>
-            <a href="/#" className="hover:text-gray-400">
-              FAQs
+            <a href="#" aria-label="Oceash">
+              Oceash
             </a>
-          </li>
-        </ul>
-
-        {/* Contact Information */}
-        <div className="text-center md:text-left mb-6 md:mb-0">
-          <p className="mb-1">Contact Us:</p>
-          <p className="mb-1">Email: info@example.com</p>
-          <p>Phone: 123-456-7890</p>
+            <a href="#" aria-label="Ugefe">
+              Ugefe
+            </a>
+            <a href="#" aria-label="Babed">
+              Babed
+            </a>
+          </div>
+          <div>
+            <b>Spotha</b>
+            <a href="#" aria-label="Miskasa">
+              Miskasa
+            </a>
+            <a href="#" aria-label="Agithe">
+              Agithe
+            </a>
+            <a href="#" aria-label="Scesha">
+              Scesha
+            </a>
+            <a href="#" aria-label="Lulle">
+              Lulle
+            </a>
+          </div>
+          <div>
+            <b>Chashakib</b>
+            <a href="#" aria-label="Chogauw">
+              Chogauw
+            </a>
+            <a href="#" aria-label="Phachuled">
+              Phachuled
+            </a>
+            <a href="#" aria-label="Tiebeft">
+              Tiebeft
+            </a>
+            <a href="#" aria-label="Ocid">
+              Ocid
+            </a>
+            <a href="#" aria-label="Izom">
+              Izom
+            </a>
+            <a href="#" aria-label="Ort">
+              Ort
+            </a>
+          </div>
+          <div>
+            <b>Athod</b>
+            <a href="#" aria-label="Pamuz">
+              Pamuz
+            </a>
+            <a href="#" aria-label="Vapert">
+              Vapert
+            </a>
+            <a href="#" aria-label="Neesk">
+              Neesk
+            </a>
+            <a href="#" aria-label="Omemanen">
+              Omemanen
+            </a>
+          </div>
         </div>
-
-        {/* Social Media Links */}
-        <div className="flex space-x-4">
-          <a href="/#" className="text-white hover:text-gray-400">
-            <FontAwesomeIcon icon={faFacebookF} className="text-2xl" />
-          </a>
-          <a href="/#" className="text-white hover:text-gray-400">
-            <FontAwesomeIcon icon={faTwitter} className="text-2xl" />
-          </a>
-          <a href="/#" className="text-white hover:text-gray-400">
-            <FontAwesomeIcon icon={faInstagram} className="text-2xl" />
-          </a>
+        <div>
+          <a
+            className="image"
+            href="https://codepen.io/z-"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              backgroundImage:
+                "url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/199011/happy.svg')",
+            }}
+            aria-label="Visit Codepen"
+          ></a>
+          <p>©2019 Not Really</p>
         </div>
-      </div>
-    </footer>
+      </Content>
+      <svg style={{ position: "fixed", top: "100vh" }}>
+        <defs>
+          <filter id="blob">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="blob"
+            />
+          </filter>
+        </defs>
+      </svg>
+    </FooterSection>
   );
 };
 
